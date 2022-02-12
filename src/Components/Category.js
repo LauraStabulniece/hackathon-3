14f30f447975d6ef4e6036b8adaf5939785073a3
AnSo <an.ragozina@gmail.com>
2022-02-12 19:30:49 +0200

    Categories (3 rows)

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,10 +1,27 @@
+import '../Components/Category.css'
+import { Link } from "react-router-dom";
 
-function Category() {
+function Category(props) {
+    const image = props.image;
+    const title = props.title;
+    const description = props.description;
+
     return (
         <div>
-
-
-
+            <hr></hr>
+            <div className="row flexcolumn">
+                <div className="col-3 columnPadding imgMain">
+                    <img src={image} />
+                </div>
+                <div className="col-9 columnPadding">
+                    <h5 className="card-title">
+                        <Link to="/"><h4>{title}</h4></Link>
+                    </h5>
+                    <p className="travel-text">
+                        <p>{description}</p>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
