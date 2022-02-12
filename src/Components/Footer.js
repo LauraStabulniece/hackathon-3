66473e3cf80e07fd364b2fe6dc49fb0053cca880
Spulga <kristinei.marcinkevicai@gmail.com>
2022-02-12 12:56:45 +0200

    add instaled fontawesome

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,34 +1,24 @@
 import SocialFollow from "./SocialFollow";
 
 function Footer() {
-    return (
-         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 mt-5 mb-5">
-                    <span class="text-muted">&copy; 2022 Copyright: Laura, Anastasija, Kristine</span>
-                    </div>
-                    <div className="col-md-6 mt-5 mb-5">
-                    <ul className="justify-content-end list-unstyled d-flex">
-                        <SocialFollow />
-              <li className="list-unstyled">
-                 <a href="https://www.facebook.com/valters.polakovs" style= {{color:"grey"}} className="fa fa-lg fa-facebook pull-right"></a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
+  return (
+    <footer className="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 p-3">
+            <span class="text-muted">
+              &copy; 2022 Copyright: Laura, Anastasija, Kristine
+            </span>
+          </div>
+          <div className="col-md-6 p-3">
+            <ul className="justify-content-end list-unstyled d-flex">
+              <SocialFollow />
             </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/Components/SocialFollow.js
+++ src/Components/SocialFollow.js
@@ -1,13 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
+function SocialFollow() {
+  return (
+    <div>
+      <a href="https://www.youtube.com/c/jamesqquick"
+        className="youtube social">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/learnbuildteach/"
+        className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/learnbuildteach"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
 
-
-function SocialFollow(){
-    return (
-        <div>
-<h3>Social Follow</h3>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default SocialFollow;
