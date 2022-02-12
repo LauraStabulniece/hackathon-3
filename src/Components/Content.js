58c4f3d6642bb8c5180e2a00472f4990cca0244d
Spulga <kristinei.marcinkevicai@gmail.com>
2022-02-12 13:50:07 +0200

    add pages

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,10 +1,18 @@
-import Carousel from './Carousel'
+import { Route, Routes } from "react-router-dom";
+import Cart from "../Pages/Cart";
+import Categories from "../Pages/Categories";
+import Home from "../Pages/Home";
+
 function Content() {
     return (
-        <div>
-
-<Carousel></Carousel>
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/cart" element={<Cart/>} />
+            </Routes>
         </div>
     )
 }
+
 export default Content;
