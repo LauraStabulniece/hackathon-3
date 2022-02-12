023bcef23a39c2eccf0062b41136c872676d9621
Laura <Laura.stabulniece@gmail.com>
2022-02-12 17:50:39 +0200

    home categories edit

---
+++ src/Components/homeCategories.js
@@ -0,0 +1,40 @@
+const homeCategories = [
+    {
+        imageUrl: '/images/1.jpg',
+        title: 'Category 1',
+        description: '“Traveling: it leaves you speechless, then turns you into a storyteller” - Ibn Battuta',
+    },
+    {
+            imageUrl: '/images/2.jpg',
+            title: 'Category 2',
+            description: '“A journey of a thousand miles begins with a single step” - Lao Tzu',
+    },
+    {
+        imageUrl: '/images/3.jpg',
+        title: 'Category 3',
+        description: '“The most beautiful thing in the world is, of course, the world itself”  Wallace Stevens',
+    }
+
+
+]
+
+
+export default homeCategories;
+
+
+
+// function HomeCategories(props) {
+
+//     const title = props.title;
+//     const img = props.img;
+//     const description = props.description;
+
+
+//     return (
+//         <div>
+           
+//         </div>
+
+//     )
+// }
+// export default HomeCategories;
