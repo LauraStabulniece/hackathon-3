7b9dd530a2f5d5dc66ce8360812423670ab98fc9
Laura <Laura.stabulniece@gmail.com>
2022-02-12 19:47:48 +0200

    up

--- src/Components/homeCategories.js
+++ src/Components/homeCategories.js
@@ -1,15 +1,15 @@
-function HomeCategories(props) {
-    const imageUrl = imageUrl.props;
-    const description = description.props;
-    return (
+// function HomeCategories(props) {
+//     const imageUrl = imageUrl.props;
+//     const description = description.props;
+//     return (
 
-        <div className="card">
-            <div className="card-body">
-                <img src={imageUrl}/>
-                <p className="card-text">{description}</p>
-            </div>
-        </div>
-    )
-}
+//         <div className="card">
+//             <div className="card-body">
+//                 <img src={imageUrl}/>
+//                 <p className="card-text">{description}</p>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default HomeCategories;
+// export default HomeCategories;
