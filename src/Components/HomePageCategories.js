fb682f2b6970244277820834a274a30b0393e7a0
Laura <Laura.stabulniece@gmail.com>
2022-02-12 19:42:13 +0200

    update home categories

--- src/Components/HomePageCategories.js
+++ src/Components/HomePageCategories.js
@@ -1,32 +1,44 @@
-import homeCategories from "./homeCategories";
+import HomeCategories from "./HomeCategories";
 import { Link } from "react-router-dom";
 
 
 function HomePageCategories() {
+    const category1 =  [{
+        imageUrl: '/images/1.jpg',
+        description: '“Traveling: it leaves you speechless, then turns you into a storyteller” - Ibn Battuta',
+    }];
+    const category2 = [{
+        imageUrl: '/images/2.jpg',
+        description: '“A journey of a thousand miles begins with a single step” - Lao Tzu',
+    }];
+    const category3 = [{
+        imageUrl: '/images/3.jpg',
+        description: '“The most beautiful thing in the world is, of course, the world itself”  Wallace Stevens',
+    }]
 
     return (
         <div>
             <h1>Categories</h1>
             <div className="row">
-                <div className="col-sm d-flex" style={{ padding: "10px", margin: "0 40px 40px 0" }}>
-                    <HomePageCategories title={homeCategories[0].imageUrl} />
+                <div className="col" style={{ padding: "10px", margin: "0 40px 40px 0" }}>
+                    <HomeCategories imageUrl={category1.imageUrl} />
                     <div className="card-body">
-                        <p><Link className="nav-link" to="">CATEGORY 1</Link></p>
-                        <HomePageCategories description={homeCategories[0].description} style={{ font: "small", text: "justify" }} />
+                        <p><Link className="nav-link" to="/categories">Category 1</Link></p>
+                        <p><HomeCategories description={category1.description} style={{ font: "small", text: "justify" }} /></p>
                     </div>
                 </div>
-                <div className="col-sm d-flex" style={{ padding: "10px", margin: "0 40px 40px 0" }}>
-                    <HomePageCategories title={homeCategories[1].imageUrl} />
+                <div className="col" style={{ padding: "10px", margin: "0 40px 40px 0" }}>
+                    <HomeCategories imageUrl={category2.imageUrl} />
                     <div className="card-body">
-                        <p><Link className="nav-link" to="">CATEGORY 2</Link></p>
-                        <HomePageCategories description={homeCategories[1].description} style={{ font: "small", text: "justify" }} />
+                        <p><Link className="nav-link" to="/categories">Category 2</Link></p>
+                        <p><HomeCategories description={category2.description} style={{ font: "small", text: "justify" }} /></p>
                     </div>
                 </div>
-                <div className="col-sm d-flex" style={{ padding: "10px", marginBottom: "40px" }}>
-                    <HomePageCategories title={homeCategories[2].imageUrl} />
+                <div className="col" style={{ padding: "10px", marginBottom: "40px" }}>
+                    <HomeCategories imageUrl={category3.imageUrl} />
                     <div className="card-body">
-                        <p><Link className="nav-link" to="">CATEGORY 3</Link></p>
-                        <HomePageCategories description={homeCategories[2].description} style={{ font: "small", text: "justify" }} />
+                        <p><Link className="nav-link" to="/categories">Category 3</Link></p>
+                        <p><HomeCategories description={category3.description} style={{ font: "small", text: "justify" }} /></p>
                     </div>
                 </div>
             </div>
